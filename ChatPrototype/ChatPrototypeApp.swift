05fef4b35f8bf51cc956fbe477ad9dc872cd4e8c
Tiffany Zhang <tzhang@lcs.on.ca>
2024-12-03 08:47:03 -0500

    Initial Commit

---
+++ ChatPrototype/ChatPrototypeApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChatPrototypeApp.swift
+//  ChatPrototype
+//
+//  Created by xinyu zhang on 2024-12-03.
+//
+
+import SwiftUI
+
+@main
+struct ChatPrototypeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
